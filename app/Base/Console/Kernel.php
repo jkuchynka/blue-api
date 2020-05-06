15ff612f284848c9d7c67e8fd903459d12c6debb
Jason Kuchynka <jason.kuchynka@gmail.com>
2020-05-05 23:30:31 -0700

    Base: Add ability for modules to register commands. Add Base ModuleConfigs command.

--- app/Base/Console/Kernel.php
+++ app/Base/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace Base\Console;
 
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Console\Command;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use Symfony\Component\Finder\Finder;
 
 class Kernel extends ConsoleKernel
 {
@@ -34,8 +39,44 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load('');
 
-        require base_path('routes/console.php');
+        // @todo: Allow for modular closure commands
+        // require base_path('routes/console.php');
+    }
+
+    /**
+     * Reimplements the load function to load commands
+     * from modules, which might have a different namespace
+     *
+     * @param  array|string $paths
+     * @return void
+     */
+    protected function load($paths)
+    {
+        // Load commands from enabled modules
+        $modules = resolve('modules');
+        $moduleConfigs = $modules->getModules();
+
+        foreach ($moduleConfigs as $module) {
+            $path = $module['paths.module'] . '/Console/Commands';
+            if (is_dir($path)) {
+                $namespace ='';// $module['namespace'];
+                foreach ((new Finder)->in($path)->files() as $command) {
+                    $command = $namespace.str_replace(
+                        ['/', '.php'],
+                        ['\\', ''],
+                        Str::after($command->getPathname(), realpath(app_path()).DIRECTORY_SEPARATOR)
+                    );
+
+                    if (is_subclass_of($command, Command::class) &&
+                        ! (new ReflectionClass($command))->isAbstract()) {
+                        Artisan::starting(function ($artisan) use ($command) {
+                            $artisan->resolve($command);
+                        });
+                    }
+                }
+            }
+        }
     }
 }
