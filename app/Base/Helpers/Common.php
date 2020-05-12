1e3424b4ff3e1a4c32310c820c1c77eadf6d469a
Jason Kuchynka <jason.kuchynka@gmail.com>
2020-05-11 23:12:28 -0700

    Base: Add Common helper with namespace methods

---
+++ app/Base/Helpers/Common.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Base\Helpers;
+
+use Illuminate\Support\Traits\Macroable;
+
+class Common
+{
+    use Macroable;
+
+    /**
+     * Gets the namespace of a class from a relative path
+     *
+     * @param  string $path
+     * @return string
+     */
+    public static function namespaceFromPath(string $path)
+    {
+        $parts = explode('/', $path);
+        // Check if contains filename
+        if (pathinfo($path, PATHINFO_EXTENSION)) {
+            array_pop($parts);
+        }
+        return implode('\\', $parts);
+    }
+
+    /**
+     * Combine a base and relative namespace
+     *
+     * @param  string $baseNamespace
+     * @param  string $namespace
+     * @return string
+     */
+    public static function namespaceCombine(string $baseNamespace, string $namespace)
+    {
+        return ltrim($baseNamespace.rtrim('\\'.$namespace, '\\'), '\\');
+    }
+}
